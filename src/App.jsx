07d6ef6fa84160e74f94405f063be7b5102bd44e
unknown <flower2dreamaom@gmail.com>
2026-04-06 22:27:19 +0700

    update something

--- src/App.jsx
+++ src/App.jsx
@@ -98,7 +98,7 @@ import {
 // --- CONFIGURATION ---
 // นำ Web App URL ที่ได้จากการ Deploy Apps Script มาวางที่นี่
 // [UPDATED] อัปเดต URL ให้ตรงกับที่คุณให้มาล่าสุด
-const GOOGLE_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbxkTcAntGAgDkAt5b_3_Zus1TDZQj5y5zGLAAx_LF5spYvIOOtF3Hpq4DyeXA6I5eWq/exec"; 
+const GOOGLE_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbyA9mnOZ_BRuVTi8mPVU0_vIWUM09Pph64k80jK5LIMFFobm2uYlNJGCC0ylRaSSlPErg/exec"; 
 // ---------------------
 
 // [ADDED] Helper Functions for Color Manipulation
